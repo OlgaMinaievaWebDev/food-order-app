777a1bb17c361945e72a0836e9c3a5aa85f0afda
Olga <minaeva9@gmail.com>
2025-03-08 12:35:33 -0500

    created custom hook for http request

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,26 +1,22 @@
-import { useEffect, useState } from "react"
 import MealItem from "./MealItem";
+import useHttp from "../hooks/useHttp";
+
+const requestConfig ={}
 
 function Meals() {
- const [meals, setMeals] = useState([]);
+  const {
+    data: loadedMeals,
+    isLoading,
+    error } = useHttp('http://localhost:3000/meals', requestConfig,[])
  
- useEffect(() => {
- async function fetchMeals() {
-  const response = await fetch('http://localhost:3000/meals');
-
-  if (!response.ok) {
-    throw new Error('Something went wrong!');
+  console.log(loadedMeals)
+  if (isLoading) {
+    return <p>Fetching meals...</p>
   }
-  const data = await response.json();
-  setMeals(data)
- 
- }
-  fetchMeals()
- }, [])
- 
+
  return (
    <ul id="meals">
-     {meals.map((meal) => (
+     {loadedMeals.map((meal) => (
        <MealItem key={meal.id} meal={meal} />
      ))}
    </ul>
